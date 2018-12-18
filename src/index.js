e3b02caa863c383babd21f011151a60bc3f09810
josuegb15 <josuegb15@gmail.com>
2018-12-17 20:28:14 -0600

    Adding a Database and Connecting Server and Databa with Prisma Bindings

--- src/index.js
+++ src/index.js
@@ -1,85 +1,38 @@
 const { GraphQLServer } = require('graphql-yoga');
-
-let links = [
-  {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL',
-  },
-];
-
-let idCount = links.length;
+const { Prisma } = require('prisma-binding');
 
 //2
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (root, args) => links.find(link => link.id === args.id),
+    feed: (root, args, context, info) => context.db.query.links({}, info),
   },
 
   Mutation: {
-    post: (root, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      };
-
-      links.push(link);
-      return link;
-    },
-
-    updateLink: (root, args) => {
-      let newLink = null;
-      links = links.map(link => {
-        if (args.id === link.id) {
-          newLink = {
-            ...link,
-            url: args.url ? args.url : link.url,
-            description: args.description ? args.description : link.description,
-          };
-
-          return newLink;
-        }
-        return link;
-      });
-
-      if (!newLink) {
-        throw 'Element not found';
-      } else {
-        return newLink;
-      }
-    },
-
-    deleteLink: (root, args) => {
-      let deletedLink = null;
-      links = links.filter(link => {
-        if (args.id === link.id) {
-          deletedLink = link;
-          return false;
-        }
-        return;
-      });
-
-      if (!deletedLink) {
-        throw 'Element not found';
-      } else {
-        return deletedLink;
-      }
-    },
+    post: (root, args, context, info) =>
+      context.db.mutation.createLink(
+        {
+          data: {
+            url: args.url,
+            description: args.description,
+          },
+        },
+        info
+      ),
   },
-
-  // Link: {
-  //   id: root => root.id,
-  //   description: root => root.description,
-  //   url: root => root.url,
-  // },
 };
 
 //3
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://us1.prisma.sh/josue-granados/database/dev',
+      secret: 'mysecret123',
+      debug: true,
+    }),
+  }),
 });
 server.start(() => console.log(`Server is runing on http://localhost:4000`));
